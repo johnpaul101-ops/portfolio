de7b913a9b049959ab47ee897c6e176ba46662dd
John Paul <cruzjpaul13@gmail.com>
2024-12-22 03:04:55 +0800

    Initial commit

--- portfolio.js
+++ portfolio.js
@@ -1,37 +1,22 @@
-
-const navBar = document.getElementById('nav_bar');
-
-window.addEventListener('scroll', () => {
-
-    if (window.scrollY >= 80) {
-        navBar.classList.add('scrolled')
-    }
-    else {
-        navBar.classList.remove('scrolled')
-    }
-})
-
-const links = document.querySelectorAll('nav_links').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').slice(1);
-        const targetElement = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: "smooth"
-        })
-    })
-})
-
-
-const menu = document.getElementById('menu');
+const menu = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('navbar__menu');
 
 menu.addEventListener('click', () => {
-    if(menu.click) {
-        navBar.classList.toggle('clicked')
-    }
+    menu.classList.toggle('is-active')
+    navLinks.classList.toggle('active')
 })
 
-
-
+const links = document.querySelectorAll('.nav__links').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent default link behavior
+        
+        // Get the target section ID from the href attribute
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        // Scroll to the target section smoothly
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
